0cc33360e889283741f13ccdbc5a125f40fd5d37
John Ingve Olsen <johningveolsen@gmail.com>
2017-03-07 23:09:20 +0100

    prevent random cow from loading itself

--- cows/random.cow.js
+++ cows/random.cow.js
@@ -1,5 +1,6 @@
 const cowsay = require('../src/cowsay');
-const cows = cowsay.list();
 
+let cows = cowsay.list();
+//remove this file from the cows list
+cows.splice(cows.indexOf('random'), 1);
 module.exports = cowsay.loadCow(cows[Math.floor(Math.random() * cows.length)]);
-
