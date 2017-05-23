99f50e3576d7d827c219643c78224faa5d8c3b7d
John Ingve Olsen <johningveolsen@gmail.com>
2017-05-23 11:51:47 +0200

    fix for the random cow

--- cows/random.cow.js
+++ cows/random.cow.js
@@ -3,4 +3,6 @@ const cowsay = require('../cowsay');
 let cows = cowsay.list();
 //remove this file from the cows list
 cows.splice(cows.indexOf('random'), 1);
-module.exports = cowsay.loadCow(cows[Math.floor(Math.random() * cows.length)]);
+module.exports = (thoughts = '\\', eyes = 'oo', tongue = '  ') => {
+    return cowsay.loadCow(cows[Math.floor(Math.random() * cows.length)])(thoughts, eyes, tongue);
+};
